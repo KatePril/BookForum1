1cf0f5e0662c79fa4eb1d5093d91602501de260e
KatePril <ket14pril14@gmail.com>
2024-04-04 14:15:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bookforum/utils/SecretKeys.kt
@@ -0,0 +1,7 @@
+package com.example.bookforum.utils
+
+object SecretKeys {
+
+    const val XRapidAPIKey = "212d0fb1bdmsh1fc92005b17d99dp11bd49jsn3913925c5f4d" // ket14pril14@gmail.con
+    const val XRapidAPIKey1 = "5890720136msh2fd5a6afc8610cdp129b2ajsn68832c889942" // lll.kpi.ua
+}

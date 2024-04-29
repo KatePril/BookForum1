549d46121257693388e9e2ab76db387809be92fc
KatePril <ket14pril14@gmail.com>
2024-04-29 14:29:00 +0300

    Update ForumViewModelProvider

--- app/src/main/java/com/example/bookforum/ui/ForumViewModelProvider.kt
+++ app/src/main/java/com/example/bookforum/ui/ForumViewModelProvider.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.bookforum.ForumApplication
 import com.example.bookforum.ui.apiUi.BooksViewModel
+import com.example.bookforum.ui.databaseUi.commentUi.viewModels.CommentViewModel
 import com.example.bookforum.ui.databaseUi.postsUI.viewModels.PostCreationViewModel
 import com.example.bookforum.ui.databaseUi.postsUI.viewModels.FeedViewModel
 import com.example.bookforum.ui.databaseUi.userUI.viewModels.LoginViewModel
@@ -18,38 +19,45 @@ object ForumViewModelProvider {
     val Factory = viewModelFactory {
         initializer {
             BooksViewModel(
-                this.createSavedStateHandle(),
-                forumApplication().container.usersRepository
+                savedStateHandle = this.createSavedStateHandle(),
+                usersRepository = forumApplication().container.usersRepository
             )
         }
         initializer {
             RegistrationViewModel(
-                forumApplication().container.usersRepository
+                usersRepository = forumApplication().container.usersRepository
             )
         }
         initializer {
             LoginViewModel(
-                forumApplication().container.usersRepository
+                usersRepository = forumApplication().container.usersRepository
             )
         }
         initializer {
             ProfileViewModel(
-                this.createSavedStateHandle(),
-                forumApplication().container.usersRepository
+                savedStateHandle = this.createSavedStateHandle(),
+                usersRepository = forumApplication().container.usersRepository
             )
         }
         initializer {
             PostCreationViewModel(
-                this.createSavedStateHandle(),
-                forumApplication().container.postsRepository,
-                forumApplication().container.usersRepository
+                savedStateHandle = this.createSavedStateHandle(),
+                postsRepository = forumApplication().container.postsRepository,
+                usersRepository = forumApplication().container.usersRepository
             )
         }
         initializer {
             FeedViewModel(
-                this.createSavedStateHandle(),
-                forumApplication().container.postsRepository,
-                forumApplication().container.usersRepository
+                savedStateHandle = this.createSavedStateHandle(),
+                postsRepository = forumApplication().container.postsRepository,
+                usersRepository = forumApplication().container.usersRepository
+            )
+        }
+        initializer {
+            CommentViewModel(
+                savedStateHandle = this.createSavedStateHandle(),
+                usersRepository = forumApplication().container.usersRepository,
+                commentsRepository = forumApplication().container.commentsRepository
             )
         }
     }

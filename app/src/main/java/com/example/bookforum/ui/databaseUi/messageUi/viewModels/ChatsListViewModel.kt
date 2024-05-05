0b3df7360699578af515948fb1686c0016d079dc
KatePril <ket14pril14@gmail.com>
2024-05-05 15:19:40 +0300

    Add ChatsListViewModel

---
+++ app/src/main/java/com/example/bookforum/ui/databaseUi/messageUi/viewModels/ChatsListViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.bookforum.ui.databaseUi.messageUi.viewModels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bookforum.data.entities.User
+import com.example.bookforum.data.repositories.UsersRepository
+import com.example.bookforum.ui.navigation.destinations.ChatsListDestination
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class ChatsListViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val usersRepository: UsersRepository
+): ViewModel() {
+
+    val userId: Int = checkNotNull(savedStateHandle[ChatsListDestination.userIdArg])
+
+    var contactsList = emptyList<User>()
+
+    init {
+        viewModelScope.launch {
+            contactsList = usersRepository
+                .getUserByNotId(userId)
+                .filterNotNull()
+                .stateIn(
+                    scope = viewModelScope
+                ).value
+        }
+    }
+
+}

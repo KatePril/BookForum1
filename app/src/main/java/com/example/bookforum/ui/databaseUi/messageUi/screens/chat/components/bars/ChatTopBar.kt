f04b9b72e87111f54a4a5c5cfe5791024fd43c7b
KatePril <ket14pril14@gmail.com>
2024-05-14 19:32:03 +0300

    Delete redundant inputs

--- app/src/main/java/com/example/bookforum/ui/databaseUi/messageUi/screens/chat/components/bars/ChatTopBar.kt
+++ app/src/main/java/com/example/bookforum/ui/databaseUi/messageUi/screens/chat/components/bars/ChatTopBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
 import com.example.bookforum.R
 import com.example.bookforum.ui.screenParts.buttons.ButtonWithIcon
 import com.example.bookforum.ui.screenParts.buttons.TopBarDefaultButtons

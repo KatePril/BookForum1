6a33629cd44077c393f2352bbb1ec5458070d712
KatePril <ket14pril14@gmail.com>
2024-05-15 17:54:05 +0300

    Update GroupEditViewModel

--- app/src/main/java/com/example/bookforum/ui/databaseUi/groupUi/viewModels/GroupEditViewModel.kt
+++ app/src/main/java/com/example/bookforum/ui/databaseUi/groupUi/viewModels/GroupEditViewModel.kt
@@ -5,10 +5,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bookforum.data.entities.GroupMember
 import com.example.bookforum.data.entities.User
 import com.example.bookforum.data.repositories.GroupMembersRepository
 import com.example.bookforum.ui.navigation.destinations.groupDestinations.GroupEditDestination
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class GroupEditViewModel(
     savedStateHandle: SavedStateHandle,
@@ -21,4 +25,14 @@ class GroupEditViewModel(
 
     var groupMembersList by mutableStateOf(emptyList<GroupMember>())
     var usersMap by mutableStateOf(emptyMap<Int, User>())
+
+    init {
+        viewModelScope.launch {
+            groupMembersList = groupMembersRepository
+                .getGroupMembersByGroupId(groupId)
+                .filterNotNull()
+                .stateIn(scope = viewModelScope)
+                .value
+        }
+    }
 }

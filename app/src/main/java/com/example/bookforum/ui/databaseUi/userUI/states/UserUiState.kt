b73965df7094f28e089a1683016649aff8dcf7f8
KatePril <ket14pril14@gmail.com>
2024-05-16 15:15:54 +0300

    Delete UserUiState.kt

--- app/src/main/java/com/example/bookforum/ui/databaseUi/userUI/states/UserUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookforum.ui.databaseUi.userUI.states
-
-data class UserUiState(
-    val userDetails: UserDetails = UserDetails()
-)
-
-/*TODO what for have I created this????*/

68ed0dde57f6c90c86e375fa586bb69070fc3c90
KatePril <ket14pril14@gmail.com>
2024-05-16 12:03:58 +0300

    Update utils

--- app/src/main/java/com/example/bookforum/utils/HashPassword.kt
+++ app/src/main/java/com/example/bookforum/utils/HashPassword.kt
@@ -1,5 +1,10 @@
 package com.example.bookforum.utils
 
+import com.example.bookforum.utils.secretItems.ALGORITHM
+import com.example.bookforum.utils.secretItems.ITERATIONS
+import com.example.bookforum.utils.secretItems.KEY_LENGTH
+import com.example.bookforum.utils.secretItems.SALT
+import com.example.bookforum.utils.secretItems.SECRET
 import java.security.spec.KeySpec
 import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory

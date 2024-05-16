b8d6347c07aab5b24fd9308a3c93a6dfecd3d095
KatePril <ket14pril14@gmail.com>
2024-05-16 15:17:40 +0300

    Delete UserByIdUiState.kt

--- app/src/main/java/com/example/bookforum/ui/databaseUi/postsUI/states/UserByIdUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bookforum.ui.databaseUi.postsUI.states
-
-import com.example.bookforum.data.entities.User
-
-data class UserByIdUiState(
-    val user: User = User(0, "", "", "")
-)
-
-/* TODO replace UserByIdUiState with mutableState of User*/

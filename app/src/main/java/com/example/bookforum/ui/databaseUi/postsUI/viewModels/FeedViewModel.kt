555adad734eab4b15bc439ef1fefde63678cc17a
KatePril <ket14pril14@gmail.com>
2024-05-16 14:59:39 +0300

    Refactor FeedViewModel

--- app/src/main/java/com/example/bookforum/ui/databaseUi/postsUI/viewModels/FeedViewModel.kt
+++ app/src/main/java/com/example/bookforum/ui/databaseUi/postsUI/viewModels/FeedViewModel.kt
@@ -1,16 +1,16 @@
 package com.example.bookforum.ui.databaseUi.postsUI.viewModels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bookforum.data.entities.Post
 import com.example.bookforum.data.repositories.PostsRepository
-import com.example.bookforum.ui.databaseUi.postsUI.states.PostsDisplayUiState
 import com.example.bookforum.ui.navigation.destinations.postDestinations.FeedDestination
-import com.example.bookforum.utils.TIMEOUT_MILLS
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class FeedViewModel(
     savedStateHandle: SavedStateHandle,
@@ -19,15 +19,17 @@ class FeedViewModel(
 
     val userId: Int = checkNotNull(savedStateHandle[FeedDestination.userIdArg])
 
-    val postsUiState: StateFlow<PostsDisplayUiState> = postsRepository
-        .getAllPosts()
-        .map { PostsDisplayUiState(it) }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLS),
-            initialValue = PostsDisplayUiState()
-        )
-    /* TODO replace PostsDisplayUiState with list*/
+    var postsList by mutableStateOf(emptyList<Post>())
+
+    init {
+        viewModelScope.launch {
+            postsList = postsRepository
+                .getAllPosts()
+                .stateIn(
+                    scope = viewModelScope
+                ).value
+        }
+    }
 
 }
 

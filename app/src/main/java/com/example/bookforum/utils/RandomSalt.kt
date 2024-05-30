7139a320ab5be3045f007bf47b6e01d77435132e
KatePril <ket14pril14@gmail.com>
2024-05-30 17:35:06 +0300

    Update RandomSalt.kt

--- app/src/main/java/com/example/bookforum/utils/RandomSalt.kt
+++ app/src/main/java/com/example/bookforum/utils/RandomSalt.kt
@@ -4,7 +4,7 @@ import kotlin.random.Random
 
 fun generateRandomSalt(): String {
     val elements = "abCD^&IfgUZ1st*()EFjeOPL_kcd.23V45whiWXYlMN69xyQzABGHuv!@7nor8#\$%mRSTpqJK,?>"
-    val saltLength = Random.nextInt(0, 10)
+    val saltLength = Random.nextInt(10, 15)
     var randomSalt = ""
     for (i in 0..saltLength) {
         randomSalt += elements[Random.nextInt(0, elements.length)]
